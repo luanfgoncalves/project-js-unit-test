e01881fecc09a8785b02ace599ef2b29068bd737
luanfgoncalves <luanfgoncalves@outlook.com>
2022-05-12 18:21:59 -0300

    exercicio 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const feedback = () => { 'Eita pessoa boa!'; };
+  if (typeof nome === 'string') {
+    const estudante = { name: nome, feedback };
+  }
+};
 
 module.exports = createStudent;
