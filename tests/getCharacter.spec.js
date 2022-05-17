6d4c9d9f711d5a886ec6ae15d12835080406dd9d
luanfgoncalves <luanfgoncalves@outlook.com>
2022-05-17 01:09:42 -0300

    iniciando exercicio 9 getCharacter

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -52,4 +52,50 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
   });
+
+  it('Verifica se a função, quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter()).toBeUndefined();
+    fail('A função não retorna undefined quando não recebe parâmetros!');
+  });
+
+  it('Verifica se a função retorna o objeto correto para o parâmetro Arya', () => {
+    const expectedOutput = {
+      arya: {
+        name: 'Arya Stark',
+        class: 'Rogue',
+        phrases: ['Not today', 'A girl has no name.'],
+      },
+    }
+    expect(Object.keys(getCharacter('arya'))).toEqual(expectedOutput);
+    fail('A função não retorna o parâmetro esperado quando recebe "arya"!');
+  });
+
+  it('Verifica se a função retorna o objeto correto para o parâmetro Brienne', () => {
+    const expectedOutput = {
+      brienne: {
+        name: 'Brienne Tarth',
+        class: 'Knight',
+        phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+      },
+    }
+    expect(Object.keys(getCharacter('brienne'))).toEqual(expectedOutput);
+    fail('A função não retorna o parâmetro esperado quando recebe "brienne"!');
+  });
+
+  it('Verifica se a função retorna o objeto correto para o parâmetro Melissandre', () => {
+    const expectedOutput = {
+      melissandre: {
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+    },
+  };
+    expect(Object.keys(getCharacter('melissandre'))).toEqual(expectedOutput);
+    fail('A função não retorna o parâmetro esperado quando recebe "melissandre"!');
+  });
+
+  it('Verifica se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.', () => {
+    fail('Teste vazio!');
+  });
+
 });
